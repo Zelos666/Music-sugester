57a2447a9940b5b2e11acd76d1fa07f94409b123
Javvad Raza <javvadraza456@gmail.com>
2024-07-06 21:18:26 +0530

    add files to project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from './components/sidebar/sidebar'
+
+const App = () => {
+  return (
+    <div>
+      <>
+        <Sidebar>
+
+        </Sidebar>
+      </>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+
+
+        </div>
+    )
+}
